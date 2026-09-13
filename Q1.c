5aeb2818e6ee9958f33fa9fcf304ac505a6809f9
ivyopare3546-png <ivyopare3546@gmail.com>
2026-09-13 14:11:09 +0000

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+printf("C PROGRAMMING FUNDAMENTALS\n");
+printf("Name: Ivy Opare\n");
+printf("Student ID: 01253640B\n");
+printf("Programme: BTECH Electrical and Electronics Engineering\n");
+printf("Welcome to C Programming\n");
+return 0;
+}
