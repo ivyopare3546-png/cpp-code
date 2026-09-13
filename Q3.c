51f1baabfe653e3f4e6924e317c20e2a48d43652
ivyopare3546-png <ivyopare3546@gmail.com>
2026-09-13 14:17:42 +0000

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    // Q3 - Arithmetic operations
+    int a = 20, b = 5;
+    printf("First number: %d\n", a);
+    printf("Second number: %d\n", b);
+    printf("Addition: %d + %d = %d\n", a, b, a + b);
+    printf("Subtraction: %d - %d = %d\n", a, b, a - b);
+    printf("Multiplication: %d * %d = %d\n", a, b, a * b);
+    printf("Division: %d / %d = %d\n", a, b, a / b);
+    printf("Modulus: %d %% %d = %d\n", a, b, a % b);
+    return 0;
+}
